d863d88c74fb8d7f513f4670873d4423ac86aa5c
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-09-02 19:37:18 +0300

    Created model Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=50, verbose_name='Наименование', **NULLABLE)
+	description = models.TextField(verbose_name='Описание')
+
+	def __str__(self):
+		return f'{self.name}'
+
+	class Meta:
+		verbose_name = 'категория'
+		verbose_name_plural = 'категории'
