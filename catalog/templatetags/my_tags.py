f1587f0b2052e9321ef920948da135012ea8835e
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-09-05 18:56:07 +0300

    Added simple tag for media path

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -2,6 +2,11 @@ from django import template
 
 register = template.Library()
 
+@register.simple_tag
+def mediapath(value):
+    if value:
+        return f'/media/{value}'
+    return '#'
 
 @register.filter()
 def mediapath(val):
