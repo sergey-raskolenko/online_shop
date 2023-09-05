001b21c978e83945dd4c03b36d1974f707514248
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-09-05 18:58:01 +0300

    Сorrected urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from catalog.views import index, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import index, contacts, item
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts)
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
+    path('item/<int:pk>', item, name='item')
 ]
