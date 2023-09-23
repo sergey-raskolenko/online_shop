ac0445ec4e3d3236d629b13b84d5de6796294048
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-09-23 22:06:27 +0300

    Created model Version

--- catalog/models.py
+++ catalog/models.py
@@ -30,3 +30,18 @@ class Product(models.Model):
 	class Meta:
 		verbose_name = 'продукт'
 		verbose_name_plural = 'продукты'
+
+
+class Version(models.Model):
+	product = models.ForeignKey('Product', on_delete=models.CASCADE)
+	version_number = models.IntegerField(verbose_name='Номер версии', unique=True)
+	version_name = models.CharField(max_length=100, verbose_name='Название версии')
+	is_active = models.BooleanField(default=False, verbose_name='Признак текущей версии')
+
+	def __str__(self):
+		return f'Название версии: {self.version_name},№{self.version_number}'
+
+	class Meta:
+		verbose_name = 'Версия'
+		verbose_name_plural = 'Версии'
+
