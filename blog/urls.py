280db8f6ebf5941541297f881603272a1f52f4e3
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-10-02 22:59:03 +0300

    Excluded some links from caching

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
 
 from blog.apps import BlogConfig
 from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
@@ -7,8 +8,8 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog'),
-    path('create_blog/', BlogCreateView.as_view(), name='create_blog'),
-    path('edit_blog/<int:pk>/', BlogUpdateView.as_view(), name='edit_blog'),
+    path('create_blog/', never_cache(BlogCreateView.as_view()), name='create_blog'),
+    path('edit_blog/<int:pk>/', never_cache(BlogUpdateView.as_view()), name='edit_blog'),
     path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete'),
     path('blogs/', BlogListView.as_view(), name='blogs'),
 ]
